2c45dd62732c75265b32097ee6566e1579260403
DESKTOP-GR83GIE\Durana <fifo989@gmail.com>
2020-12-08 17:41:20 +0100

    Add project files.

---
+++ Rocnikovy/Rocnikovy.cpp
@@ -0,0 +1,27 @@
+// Rocnikovy.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a;
+	string b[] = { "auto","skola","mango","dom","helikoptera","pes" }; //velkost=6,maxIndex=5
+	cout << "Vitaj v hre Obesenec" << endl;
+	cout << endl;
+	cout << "Moznosti:" << endl;
+	cout << "1.Hrat" << endl;
+	cout << "2.Ukoncit" << endl;
+	cout << "Co chces:";
+	cin >> a;	
+	cout << endl;
+	if (a==1)
+		
+	
+
+
+	
+		
+}
+
